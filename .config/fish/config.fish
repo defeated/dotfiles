0071d0c69f3039af6719ac1259911509a821de15
eddie cianci <defeated@users.noreply.github.com>
2026-09-05 18:12:17 -0400

    rewrite: cachyOS, mise bootstrap, niri, noctalia
    2026 might really be the year of linux on the desktop ;)

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+starship init fish | source
+mise activate fish | source
+zoxide init fish --cmd cd | source
+
+alias cat='bat --paging=never'
+alias z='__zoxide_z'
+alias zi='__zoxide_zi'
